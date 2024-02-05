a15d4397c3978776f836dd712f4848e1d36b04a9
aclercbeaufo <alexis.clerc-beaufort@etu.u-bordeaux.fr>
2024-02-05 11:43:27 +0100

    ex7 fait

--- intentions/app/src/main/java/iut/exercices/intentionsend/MainActivity.java
+++ intentions/app/src/main/java/iut/exercices/intentionsend/MainActivity.java
@@ -1,23 +1,36 @@
 package iut.exercices.intentionsend;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityResultLauncher<Intent> myIntentsLauncher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myIntentsLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Intent data = result.getData();
+            }
+        });
     }
 
-    protected void onSend(View view) {
+    public void onSend(View view) {
         EditText editText = findViewById(R.id.messageEditText);
         String message = editText.getText().toString();
         System.out.println("Message : " + message);
+        Intent intent = new Intent(this, intentionRecieve.class);
+        intent.putExtra("message", message);
+        myIntentsLauncher.launch(intent);
         editText.setText("");
     }
 }
--- intentions/app/src/main/java/iut/exercices/intentionsend/intentionRecieve.java
+++ intentions/app/src/main/java/iut/exercices/intentionsend/intentionRecieve.java
@@ -2,7 +2,9 @@ package iut.exercices.intentionsend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class intentionRecieve extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class intentionRecieve extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intention_recieve);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        TextView textView = findViewById(R.id.textTarget);
+        textView.setText(message);
     }
+
+
 }
