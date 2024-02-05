826b1416871e0c6ad19227e77ac028d7de589ae3
aclercbeaufo <alexis.clerc-beaufort@etu.u-bordeaux.fr>
2024-02-05 09:47:39 +0100

    2nd activité ajoutée

---
+++ intentions/app/src/main/java/iut/exercices/intentionsend/intentionRecieve.java
@@ -0,0 +1,14 @@
+package iut.exercices.intentionsend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class intentionRecieve extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intention_recieve);
+    }
+}
