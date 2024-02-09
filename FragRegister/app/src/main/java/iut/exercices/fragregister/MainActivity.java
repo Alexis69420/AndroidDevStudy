6eb937aedbe4a32cfb9ee735cb481d813a2025ae
aclercbeaufo <alexis.clerc-beaufort@etu.u-bordeaux.fr>
2024-02-09 17:20:59 +0100

    fonctions importées du projet précédent

--- FragRegister/app/src/main/java/iut/exercices/fragregister/MainActivity.java
+++ FragRegister/app/src/main/java/iut/exercices/fragregister/MainActivity.java
@@ -1,8 +1,15 @@
 package iut.exercices.fragregister;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView nameView = findViewById(R.id.nameView);
+        TextView firstnameView = findViewById(R.id.firstnameView);
+        TextView phNumberView = findViewById(R.id.phNumberView);
+        TextView hNumberView = findViewById(R.id.hNumberView);
+        TextView streetView = findViewById(R.id.streetView);
+        TextView pCodeView = findViewById(R.id.pCodeView);
+        TextView cityView = findViewById(R.id.cityView);
+        
+    }
+
+    public void changeUserInfo(View view) {
+    }
+
+    public void changeUserAddress(View view) {
     }
 }
