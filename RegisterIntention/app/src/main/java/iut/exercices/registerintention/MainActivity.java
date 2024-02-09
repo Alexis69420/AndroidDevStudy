812f3788a7f7cfe039f999ffb38f858f633420d7
aclercbeaufo <alexis.clerc-beaufort@etu.u-bordeaux.fr>
2024-02-09 12:05:44 +0100

    version non fonctionnelle

--- RegisterIntention/app/src/main/java/iut/exercices/registerintention/MainActivity.java
+++ RegisterIntention/app/src/main/java/iut/exercices/registerintention/MainActivity.java
@@ -1,5 +1,7 @@
 package iut.exercices.registerintention;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,8 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] userInfo = new String[7];
+
+    boolean firstTime = true;
 
     private ActivityResultLauncher<Intent> myIntentsLauncher;
 
@@ -16,11 +23,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myIntentsLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-            if (result.getResultCode() == RESULT_OK) {
-                Intent data = result.getData();
-            }
-        });
+        TextView nameText = findViewById(R.id.nameView);
+        TextView surnameText = findViewById(R.id.firstnameView);
+        TextView telephoneText = findViewById(R.id.pCodeView);
+        TextView addressNumberText = findViewById(R.id.hNumberView);
+        TextView addressNameText = findViewById(R.id.streetView);
+        TextView addressPostalText = findViewById(R.id.pCodeView);
+        TextView cityText = findViewById(R.id.cityView);
+
+        // initialisation du lanceur et traitement des réponses aux intentions
+        myIntentsLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            for (String key : data.getExtras().keySet()) {
+                                switch (key) {
+                                    case "name":
+                                        nameText.setText(data.getExtras().getString("name"));
+                                        break;
+                                    case "surname":
+                                        surnameText.setText(data.getExtras().getString("surname"));
+                                        break;
+                                    case "telephone":
+                                        telephoneText.setText(data.getExtras().getString("telephone"));
+                                        break;
+                                    case "addressNumber":
+                                        addressNumberText.setText(data.getExtras().getString("addressNumber"));
+                                        break;
+                                    case "addressName":
+                                        addressNameText.setText(data.getExtras().getString("addressName"));
+                                        break;
+                                    case "addressPostal":
+                                        addressPostalText.setText(data.getExtras().getString("addressPostal"));
+                                        break;
+                                    case "city":
+                                        cityText.setText(data.getExtras().getString("city"));
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                });
     }
 
     public void changeUserInfo(View view) {
--- RegisterIntention/app/src/main/java/iut/exercices/registerintention/Modify.java
+++ RegisterIntention/app/src/main/java/iut/exercices/registerintention/Modify.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Modify extends AppCompatActivity {
 
@@ -21,5 +22,25 @@ public class Modify extends AppCompatActivity {
                 Intent data = result.getData();
             }
         });
+    }
+
+    public void onCancel(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        myIntentsLauncher.launch(intent);
+    }
+
+    public void onModify(View view) {
+        EditText name = findViewById(R.id.name);
+        EditText firstname = findViewById(R.id.firstname);
+        EditText phNumber = findViewById(R.id.phNumber);
+        String nameStr = name.getText().toString();
+        String firstnameStr = firstname.getText().toString();
+        String phNumberStr = phNumber.getText().toString();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("name", nameStr);
+        intent.putExtra("firstname", firstnameStr);
+        intent.putExtra("phNumber", phNumberStr);
+        intent.putExtra("class", "modify");
+        myIntentsLauncher.launch(intent);
     }
 }
