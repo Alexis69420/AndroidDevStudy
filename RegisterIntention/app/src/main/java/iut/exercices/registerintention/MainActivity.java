3d7f89223236f60aebe8eb7645c0621af7f69796
aclercbeaufo <alexis.clerc-beaufort@etu.u-bordeaux.fr>
2024-02-09 09:49:12 +0100

    main activity done

--- RegisterIntention/app/src/main/java/iut/exercices/registerintention/MainActivity.java
+++ RegisterIntention/app/src/main/java/iut/exercices/registerintention/MainActivity.java
@@ -1,14 +1,35 @@
 package iut.exercices.registerintention;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<Intent> myIntentsLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myIntentsLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Intent data = result.getData();
+            }
+        });
+    }
+
+    protected void changeUserInfo(View view) {
+        Intent intent = new Intent(this, Modify.class);
+        myIntentsLauncher.launch(intent);
+    }
+
+    protected void changeUserAddress(View view) {
+        Intent intent = new Intent(this, UserInfo.class);
+        myIntentsLauncher.launch(intent);
     }
 }
